eb14c910e90999cb98306f6c5c122345fe951dc4
Hasan Alkiani <halkinai2@huskers.unl.edu>
2023-08-24 09:58:44 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: hasan Alkinani
+ *         halkinani2@huskers.unl.edu
+ * Date: 2023/08/22
+ *
+ * A print my name program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hasan Alkiani\n");
+
+  return 0;
+}
